69be9db27ed3f8e6ba15fc6346caf57dd6a297a6
Oucema Jlaiel <oucema@movida-mail.com>
2021-10-20 14:57:10 +0200

    Initial commit

---
+++ landmarks/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  landmarks
+//
+//  Created by Oucema on 20/10/2021.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Peut pas trouver le fichier")
+    }
+    
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Peut pas charger le fichier")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Peut pas parser le fichier en json")
+    }
+}
---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Oucema on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
